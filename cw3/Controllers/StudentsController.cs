75a615f7431c46a9f0b3cb05998437191c65b57e
Jakub Bogusławski <s18312@pjwstk.edu.pl>
2020-03-19 01:55:17 +0100

    Drugi Commit

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw3.Controllers
@@ -10,12 +11,55 @@ namespace cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet("{id}")]
-        public string GetStudents([FromRoute]int id)
+
+        
+
+        [HttpGet]
+        public string GetStudents(string orderBy)
         {
-            return $"Chce informacje o studencie o ID:  { id}";
+            return "Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+        } 
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
         }
 
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, [FromBody]Student stud)
+        {
+            if(stud.IdStudent == id)
+            {
+                stud.FirstName = "Tymoteusz";
+            }
+            return Ok("Aktualizacja dokończona " + stud.FirstName + " " + stud.LastName);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id, [FromBody]Student stud)
+        {
+            if (stud.IdStudent == id)
+            {
+                stud = null;
+            }
+            return Ok("Usuwanie ukończone");
+        }
 
     }
 }
